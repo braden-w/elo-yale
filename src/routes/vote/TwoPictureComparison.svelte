cd9720862d5efe2394b13e5e3bd663d68ed508d9
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 15:20:24 -0800

    Divider

---
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="flex flex-col gap-4">
+	<img src="https://picsum.photos/200/300" alt="" />
+	<div class="relative">
+		<div class="absolute inset-0 flex items-center" aria-hidden="true">
+			<div class="w-full border-t border-gray-300" />
+		</div>
+		<div class="relative flex justify-center">
+			<span class="bg-white px-2 text-sm text-gray-500">or</span>
+		</div>
+	</div>
+</div>
