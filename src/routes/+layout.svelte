3d84153cecd6caa9f6990cbc4bf70c8093e8d51a
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 14:46:16 -0800

    Layout and Lock

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "../app.postcss";
+    import type { LayoutData } from './$types';
+
+    export let data: LayoutData;
+</script>
+
+<div class="flex bg-slate-800">
+<h1 class="text-2xl text-slate-100">Elo Yale</h1>
+</div>
+
+<slot></slot>
