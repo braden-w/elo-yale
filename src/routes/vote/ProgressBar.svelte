38c495e31ad61140ec6d7ab0bf85f7787138d804
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 15:31:43 -0800

    ProgressBar: create

---
+++ src/routes/vote/ProgressBar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	// Type for number between 0 and 100
+	export let progress: number;
+</script>
+
+<div class="w-full rounded-full h-1.5 mb-4 bg-slate-500">
+	<div class="h-1.5 rounded-full bg-slate-700" style="width: {progress}%" />
+</div>
