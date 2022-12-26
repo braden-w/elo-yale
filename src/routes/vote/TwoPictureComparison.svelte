acdb824c66f0118d3edb8e4a4cd552df68b9e4ee
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 15:21:53 -0800

    TwoPictureComparison

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
+	import Divider from './Divider.svelte';
+
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
 <div class="flex flex-col gap-4">
-	<img src="https://picsum.photos/200/300" alt="" />
-	<div class="relative">
-		<div class="absolute inset-0 flex items-center" aria-hidden="true">
-			<div class="w-full border-t border-gray-300" />
-		</div>
-		<div class="relative flex justify-center">
-			<span class="bg-white px-2 text-sm text-gray-500">or</span>
-		</div>
-	</div>
+	<img src="https://picsum.photos/200/300" alt="" class="rounded-2xl" />
+	<Divider />
+	<img src="https://picsum.photos/200/300" alt="" class="rounded-2xl" />
 </div>
