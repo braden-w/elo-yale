a4579ed3bb237594cfef14a19ba0e377aa76cb35
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 02:15:46 -0800

    Tracking

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -8,6 +8,6 @@
 </script>
 
 <div class="mx-auto flex h-full flex-col items-center gap-6 p-6 sm:max-w-4xl">
-	<h1 class="text-xl text-slate-700">Which Residential College is Best?</h1>
+	<h1 class="text-xl tracking-wide text-slate-700">Which Residential College is Best?</h1>
 	<TwoPictureComparison />
 </div>
