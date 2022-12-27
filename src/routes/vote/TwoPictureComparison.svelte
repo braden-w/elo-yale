cd826cb1415d00ebaf603eb520f7ca93facadc08
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 18:38:40 -0800

    Use Colleges

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -4,26 +4,19 @@
 	import type { PageData } from './$types';
 	import HorizontalDivider from './HorizontalDivider.svelte';
 	import VerticalDivider from './VerticalDivider.svelte';
+	import { colleges } from 'src/lib/colleges';
 
 	export let data: PageData;
 </script>
 
-<div class="flex flex-col sm:flex-row gap-4 w-full h-full align-center justify-center">
+<div class="align-center flex h-full w-full flex-col justify-center gap-4 sm:flex-row">
 	<div class="h-full w-full">
-		<img
-			src="/colleges/Rustic/DALL·E 2022-12-26 10.18.46 - extremely detailed artstation high quality concept art image of a rustic autumn view of a Yale Residential College_ 8K UHD , digital art, Archdaily.png"
-			alt=""
-			class="rounded-2xl object-cover"
-		/>
+		<img src={colleges['Benjamin Franklin']} alt="" class="rounded-2xl object-cover" />
 	</div>
 	<HorizontalDivider class="sm:hidden" />
 	<VerticalDivider class="hidden sm:flex" />
 	<div class="h-full w-full">
-		<img
-			src="/colleges/Rustic/DALL·E 2022-12-26 10.18.46 - extremely detailed artstation high quality concept art image of a rustic autumn view of a Yale Residential College_ 8K UHD , digital art, Archdaily.png"
-			alt=""
-			class="rounded-2xl object-cover"
-		/>
+		<img src={colleges['Pauli Murray']} alt="" class="rounded-2xl object-cover" />
 	</div>
 </div>
 
