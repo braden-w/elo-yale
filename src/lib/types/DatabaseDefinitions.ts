74cf68c62482528483fbb7c75725d93cb925d029
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 09:51:33 -0800

    Types

---
+++ src/lib/types/DatabaseDefinitions.ts
@@ -0,0 +1,46 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      votes: {
+        Row: {
+          id: string
+          created_at: string | null
+          winner: string
+          loser: string
+          user_id: string | null
+        }
+        Insert: {
+          id: string
+          created_at?: string | null
+          winner: string
+          loser: string
+          user_id?: string | null
+        }
+        Update: {
+          id?: string
+          created_at?: string | null
+          winner?: string
+          loser?: string
+          user_id?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
