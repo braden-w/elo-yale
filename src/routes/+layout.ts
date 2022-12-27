61857eeb2bd81c9aaa6eb4df8ac4ca6331399060
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 23:09:34 -0800

    Fix: Function

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,7 @@
 import type { LayoutLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-import { reloadEntriesStore } from '$lib/entries/store';
 
 export const load: LayoutLoad = async (event) => {
 	const { session } = await getSupabase(event);
-	await reloadEntriesStore();
 	return { session };
 };
