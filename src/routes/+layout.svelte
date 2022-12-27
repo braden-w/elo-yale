c352096ac4add1991bd5d094475dda4585fa65f8
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 18:56:41 -0800

    +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,9 @@
 	export let data: LayoutData;
 </script>
 
-<div class="flex bg-slate-800 align-center justify-center">
-	<h1 class="text-2xl text-slate-100">Elo Yale</h1>
+<div class="flex h-full w-full flex-col items-center justify-center bg-slate-100">
+	<div class="align-center flex w-full justify-center bg-slate-800">
+		<h1 class="text-2xl text-slate-100">Elo Yale</h1>
+	</div>
+	<slot />
 </div>
-
-<slot />
