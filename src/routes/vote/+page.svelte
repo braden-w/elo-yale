2e92b7629656fc01efe8b649ef2c2e2508d1014e
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 18:38:52 -0800

    Max Width

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -6,7 +6,7 @@
 	export let data: PageData;
 </script>
 
-<div class="flex flex-col p-4 gap-2 max-w-2xl mx-auto">
+<div class="flex flex-col p-4 gap-2 max-w-4xl mx-auto">
 	<h1 class="text-slate-500">Which Residential College is Best?</h1>
 	<TwoPictureComparison />
 </div>
