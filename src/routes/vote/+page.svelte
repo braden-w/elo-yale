9facbfa8811bc48884a3d3fd1ed7cfb600c888ec
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 09:42:29 -0800

    Only show kbd on desktop

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -3,6 +3,7 @@
 
 	import type { PageData } from './$types';
 	import { collegePairs } from '$lib/colleges';
+	import KeyboardShortcutAlert from './KeyboardShortcutAlert.svelte';
 
 	export let data: PageData;
 </script>
@@ -10,4 +11,5 @@
 <div class="mx-auto flex h-full flex-col items-center gap-6 p-6 sm:max-w-4xl">
 	<h1 class="text-2xl tracking-wide text-slate-700">Which Residential College is Best?</h1>
 	<TwoPictureComparison />
+	<div class="hidden sm:block"><KeyboardShortcutAlert /></div>
 </div>
