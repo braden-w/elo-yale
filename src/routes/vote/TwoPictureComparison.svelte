4d3b800e1c84d1d47c1aa0cf68b9aff05d451136
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 19:09:28 -0800

    Feat: Compare Colleges

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -18,11 +18,18 @@
 	<HorizontalDivider class="sm:hidden" />
 	<VerticalDivider class="hidden sm:flex" />
 	<div class="h-full w-full">
-		<img
-			src={colleges[collegeTwo]}
-			alt="Picture of {collegeTwo}"
-			class="rounded-2xl object-cover"
-		/>
+		<div class="relative overflow-hidden rounded-lg">
+			<img
+				src={colleges[collegeTwo]}
+				alt="Picture of {collegeTwo}"
+				class="rounded-2xl object-cover"
+			/>
+			<div
+				class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white hover:bg-opacity-50 hover:opacity-90"
+			>
+				<p class="text-4xl font-bold tracking-wider text-white">{collegeTwo}</p>
+			</div>
+		</div>
 	</div>
 </div>
 
