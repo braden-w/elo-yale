042dfe57790bf972a301f91681414f82473e30f9
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 19:32:19 -0800

    Remove Photos in Favor of Imgur

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -28,18 +28,20 @@
 	<HorizontalDivider class="sm:hidden" />
 	<VerticalDivider class="hidden sm:flex" />
 	<div class="h-full w-full">
-		<div class="relative overflow-hidden rounded-2xl">
+		<button class="relative overflow-hidden rounded-2xl">
 			<img
 				src={colleges[collegeTwo]}
 				alt="Picture of {collegeTwo}"
 				class="rounded-2xl object-cover"
 			/>
 			<div
-				class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white hover:bg-opacity-50 hover:opacity-90"
+				class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white transition hover:bg-opacity-50 hover:opacity-90 {animate
+					? 'duration-150 ease-in-out hover:-translate-y-1 hover:scale-105'
+					: ''}"
 			>
 				<p class="text-4xl font-bold tracking-wider text-white">{collegeTwo}</p>
 			</div>
-		</div>
+		</button>
 	</div>
 </div>
 
