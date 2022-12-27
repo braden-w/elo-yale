e3c93a54b8278937f50c9423b1b1b5448f564de7
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 09:28:56 -0800

    Min H Screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
 	export let data: LayoutData;
 </script>
 
-<div class="flex h-screen w-full flex-col items-center bg-slate-100">
+<div class="flex min-h-screen w-full flex-col items-center bg-slate-100">
 	<Toaster />
 	<div class="align-center flex w-full justify-center bg-slate-800">
 		<a href="/"><h1 class="text-2xl tracking-widest text-slate-100">Elo Yale</h1></a>
