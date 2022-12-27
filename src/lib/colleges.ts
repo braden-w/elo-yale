6d212fc3ad52581b80571b7700d7e5d5be86c804
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 18:31:43 -0800

    colleges: create

---
+++ src/lib/colleges.ts
@@ -0,0 +1,17 @@
+export const colleges = {
+	'Benjamin Franklin': 'https://i.imgur.com/5LGFYXT.png',
+	'Pauli Murray': 'https://i.imgur.com/pMEXfFk.png',
+	'Grace Hopper': 'https://i.imgur.com/jKe7kLj.png',
+	Trumbull: 'https://i.imgur.com/ykuMs9b.png',
+	// "https://i.imgur.com/jzDlVug.png"
+	Pierson: 'https://i.imgur.com/e7DlOO9.png',
+	'Timothy Dwight': 'https://i.imgur.com/eiBFuvo.png',
+	'Ezra Stiles': 'https://i.imgur.com/s1VjYG4.png',
+	Branford: 'https://i.imgur.com/Q7JeCCL.png',
+	'Jonathan Edwards': 'https://i.imgur.com/FmywKDR.png',
+	Saybrook: 'https://i.imgur.com/mlgjdon.png',
+	Morse: 'https://i.imgur.com/chwUE5a.png',
+	Davenport: 'https://i.imgur.com/IZWKoLd.png',
+	Silliman: 'https://i.imgur.com/8F8bntY.png',
+	Berkeley: 'https://i.imgur.com/lIwkjK4.png'
+};
