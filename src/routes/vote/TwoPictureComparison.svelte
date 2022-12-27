b54ede8c6ec130fed2fbcfa2612b16b0527d6abb
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 19:24:30 -0800

    Feat: Button with animations

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -9,18 +9,18 @@
 
 <div class="flex h-full w-full flex-col gap-4 sm:flex-row">
 	<div class="h-full w-full">
-		<div class="relative overflow-hidden rounded-2xl">
+		<button class="relative overflow-hidden rounded-2xl">
 			<img
 				src={colleges[collegeOne]}
 				alt="Picture of {collegeOne}"
 				class="rounded-2xl object-cover"
 			/>
 			<div
-				class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white hover:bg-opacity-50 hover:opacity-90"
+				class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-opacity-50 hover:opacity-90"
 			>
 				<p class="text-4xl font-bold tracking-wider text-white">{collegeOne}</p>
 			</div>
-		</div>
+		</button>
 	</div>
 	<HorizontalDivider class="sm:hidden" />
 	<VerticalDivider class="hidden sm:flex" />
