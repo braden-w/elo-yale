58d789a4213a140dbc6bd7e2732e42213233600c
Braden Wong <bmw02002turbo@gmail.com>
2022-12-26 19:00:54 -0800

    TwoPictureComparison

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 	import ProgressBar from './ProgressBar.svelte';
-
-	import type { PageData } from './$types';
 	import HorizontalDivider from './HorizontalDivider.svelte';
 	import VerticalDivider from './VerticalDivider.svelte';
-	import { colleges } from '$lib/colleges';
-
-	export let data: PageData;
+	import { colleges, type College } from '$lib/colleges';
+	export let collegeOne: College = 'Benjamin Franklin';
+	export let collegeTwo: College = 'Pauli Murray';
 </script>
 
-<div class="align-center flex h-full w-full flex-col justify-center gap-4 sm:flex-row">
+<div class="flex h-full w-full flex-col gap-4 sm:flex-row">
 	<div class="h-full w-full">
-		<img src={colleges['Benjamin Franklin']} alt="" class="rounded-2xl object-cover" />
+		<img
+			src={colleges[collegeOne]}
+			alt="Picture of {collegeOne}"
+			class="rounded-2xl object-cover"
+		/>
 	</div>
 	<HorizontalDivider class="sm:hidden" />
 	<VerticalDivider class="hidden sm:flex" />
 	<div class="h-full w-full">
-		<img src={colleges['Pauli Murray']} alt="" class="rounded-2xl object-cover" />
+		<img
+			src={colleges[collegeTwo]}
+			alt="Picture of {collegeTwo}"
+			class="rounded-2xl object-cover"
+		/>
 	</div>
 </div>
 
