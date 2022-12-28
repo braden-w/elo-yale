98b7536ab2c13436e729d760c2f9a9d0e71ca1b0
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 23:03:01 -0800

    Bind

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -10,10 +10,10 @@
 
 <div class="mx-auto flex h-full flex-col items-center gap-6 p-6 sm:max-w-4xl">
 	<h1 class="text-2xl tracking-wide text-slate-700">Which Residential College is Best?</h1>
-	{#if remainingCollegePairs === null}
+	{#if remainingCollegePairs === null || remainingCollegePairs.length === 0}
 		<p class="text-xl text-slate-700">You've voted on all the pairs!</p>
 	{:else}
-		<TwoPictureComparison {remainingCollegePairs} />
+		<TwoPictureComparison bind:remainingCollegePairs />
 	{/if}
 	<div class="hidden sm:block"><KeyboardShortcutAlert /></div>
 </div>
