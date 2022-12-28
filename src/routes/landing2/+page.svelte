c08e66a3d4a3695c9236ce5457c7efdb20dba715
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 20:31:00 -0800

    Layout

--- src/routes/landing2/+page.svelte
+++ src/routes/landing2/+page.svelte
@@ -19,20 +19,17 @@
 			class="h-full max-h-screen w-screen object-cover"
 		/>
 		<div
-			class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white"
+			class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40"
 		>
-			<div
-				class="flex flex-col items-center gap-4  transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-opacity-50 hover:opacity-90 "
-			>
-				<h1 class="text-5xl tracking-wider text-white">
+			<div class="flex flex-col gap-4 text-white">
+				<!-- transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-opacity-50 hover:opacity-90  -->
+				<h1 class="max-w-3xl text-5xl tracking-wide">
 					Solve the Most Important Question on Campus
 				</h1>
-				<h2 class="text-4xl tracking-wider text-white">
-					Which Residential College is Actually the Best?
-				</h2>
+				<h2 class="text-2xl">Which Residential College is Actually the Best?</h2>
 				<button
 					on:click={signInWithGoogle}
-					class="peer inline-flex justify-center rounded-xl border border-gray-300 bg-slate-700 py-2 px-4 text-slate-100 shadow-sm hover:bg-gray-700"
+					class="peer inline-flex justify-center rounded-lg bg-slate-800 py-2 px-4 shadow-sm hover:bg-gray-700"
 				>
 					<!-- <GoogleIcon /> -->
 					<span class="tracking-wider">Sign in with yale.edu to find out</span>
