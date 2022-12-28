6498f5fffd80cd1b3f15dce0b128b1c5d642970f
Braden Wong <bmw02002turbo@gmail.com>
2022-12-28 02:28:07 -0800

    +page

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -5,10 +5,12 @@
 	const { leaderboard } = data;
 </script>
 
-{#each leaderboard as { college, wins, losses }}
+{#each leaderboard as { college, wins, losses, win_rate }}
+	{@const total = wins ?? 0 + losses ?? 0}
 	<div class="flex flex-col items-center gap-4">
 		<h1 class="text-2xl tracking-wide text-slate-700">{college}</h1>
 		<p class="text-center text-slate-500">Wins: {wins}</p>
-		<p class="text-center text-slate-500">Losses: {losses}</p>
+		<p class="text-center text-slate-500">Losses: {total}</p>
+		<p class="text-center text-slate-500">Winrate: {win_rate}</p>
 	</div>
 {/each}
