a15a0b3739389b7baf8541fcbb2175f4ca4ea889
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 21:57:09 -0800

    Animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,8 +17,8 @@
 		<img
 			src={collegeToImage['Branford']}
 			alt="Branford"
-			class="h-full max-h-screen w-screen object-cover {hover
-				? '-translate-y-1 scale-105 transition duration-150 ease-in-out'
+			class="h-full max-h-screen w-screen object-cover transition duration-150 ease-in-out {hover
+				? '-translate-y-1 scale-105'
 				: ''}"
 		/>
 		<div
