3c652c9f63a1f97cf39d9f6006babe43bea59d76
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 23:27:18 -0800

    Comment out debug

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -9,8 +9,8 @@
 	let pairNumber = 0;
 </script>
 
-{JSON.stringify(remainingCollegePairs)}
-{pairNumber}
+<!-- {JSON.stringify(remainingCollegePairs)} -->
+<!-- {pairNumber} -->
 <div class="mx-auto flex h-full flex-col items-center gap-6 p-6 sm:max-w-4xl">
 	<h1 class="text-2xl tracking-wide text-slate-700">Which Residential College is Best?</h1>
 	{#if remainingCollegePairs === null || remainingCollegePairs.length === pairNumber}
