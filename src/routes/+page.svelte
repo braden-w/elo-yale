d64570e320922f7cbe35c282996365046df6d855
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 22:53:49 -0800

    lignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@ Which Residential College is <span class="tracking-widest">Actually</span> the B
 				<div class="flex gap-4">
 				<button
 					on:click={signInWithGoogle}
-					class="inline-flex w-fit justify-center rounded-lg bg-stone-700 py-3 px-6 shadow-xl hover:shadow-2xl"
+					class="inline-flex w-fit justify-center rounded-lg bg-stone-200 text-stone-700 py-3 px-6 shadow-xl hover:shadow-2xl"
 					on:mouseover={() => (hover = true)}
 					on:mouseout={() => (hover = false)}
 					on:focus={() => (hover = true)}
@@ -47,8 +47,12 @@ Which Residential College is <span class="tracking-widest">Actually</span> the B
 				>
 					<!-- <GoogleIcon /> -->
 					<span class="tracking-wider">Sign in with yale.edu to find out</span>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+</svg>
+
 				</button>
-				<button class="inline-flex w-fit justify-center rounded-lg bg-stone-700 px-2 shadow-xl hover:shadow-2xl" 				on:click={() => {
+				<button class="inline-flex w-fit items-center justify-center rounded-lg bg-stone-700 px-2 shadow-xl hover:shadow-2xl" 				on:click={() => {
 					backgroundCollegeIndex = (backgroundCollegeIndex + 1) % slideColleges.length
 				}}>
 				<PlaceholderImage />
