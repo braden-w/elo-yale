6ff463bdeafa63d7551955597589e021ed4ae120
Braden Wong <bmw02002turbo@gmail.com>
2022-12-28 12:19:10 -0800

    Results after vote

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -12,15 +12,24 @@
 <!-- {JSON.stringify(remainingCollegePairs)} -->
 <!-- {pairNumber} -->
 <div class="mx-auto flex h-full flex-col items-center gap-6 p-6 sm:max-w-4xl">
-	<div class="flex flex-col items-center gap-2">
-		<h1 class="text-lg tracking-wide text-slate-700 sm:text-2xl">
-			Which Residential College is Best?
-		</h1>
-		<h2 class="text-xs tracking-wide text-slate-700">Your vote matters – please be honest!</h2>
-	</div>
 	{#if remainingCollegePairs === null || remainingCollegePairs.length === pairNumber}
-		<p class="text-xl text-slate-700">You've voted on all the pairs!</p>
+		<div class="flex flex-col items-center gap-2">
+			<h1 class="text-lg tracking-wide text-slate-700 sm:text-2xl">
+				You've voted on all the pairs!
+			</h1>
+			<h2 class="tracking-wide text-slate-700">
+				<a href="/leaderboard" class="text-xs text-sky-700 hover:text-sky-900">Go to leaderboard</a>
+			</h2>
+		</div>
 	{:else}
+		<div class="flex flex-col items-center gap-2">
+			<h1 class="text-lg tracking-wide text-slate-700 sm:text-2xl">
+				Which Residential College is Best?
+			</h1>
+			<h2 class="text-xs tracking-wide text-slate-700">
+				Your vote matters – please be honest! Results are displayed after you vote.
+			</h2>
+		</div>
 		<TwoPictureComparison bind:pairNumber bind:remainingCollegePairs />
 	{/if}
 	<div class="hidden sm:block"><KeyboardShortcutAlert /></div>
