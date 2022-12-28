da18eb225f32c6ad0619fd258c3a434ae70fa6bb
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 20:12:36 -0800

    Sizing

--- src/routes/landing2/+page.svelte
+++ src/routes/landing2/+page.svelte
@@ -13,12 +13,18 @@
 
 <div class="h-full w-full">
 	<div class="relative overflow-hidden">
-		<img src={collegeToImage['Branford']} alt="Branford" class="h-screen w-screen object-cover" />
-		<button
-			class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-opacity-50 hover:opacity-90 "
+		<img
+			src={collegeToImage['Branford']}
+			alt="Branford"
+			class="h-full max-h-screen w-screen object-cover"
+		/>
+		<div
+			class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-40 text-center text-white"
 		>
-			<div class="flex flex-col items-center gap-4">
-				<h1 class="text-6xl font-bold tracking-wider text-white">
+			<div
+				class="flex flex-col items-center gap-4  transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-opacity-50 hover:opacity-90 "
+			>
+				<h1 class="text-5xl tracking-wider text-white">
 					Solve the Most Important Question on Campus
 				</h1>
 				<h2 class="text-4xl tracking-wider text-white">
@@ -26,12 +32,12 @@
 				</h2>
 				<button
 					on:click={signInWithGoogle}
-					class="inline-flex w-full justify-center rounded-xl border border-gray-300 bg-slate-700 py-2 px-4 text-slate-100 shadow-sm hover:bg-gray-700"
+					class="peer inline-flex justify-center rounded-xl border border-gray-300 bg-slate-700 py-2 px-4 text-slate-100 shadow-sm hover:bg-gray-700"
 				>
 					<!-- <GoogleIcon /> -->
 					<span class="tracking-wider">Sign in with yale.edu to find out</span>
 				</button>
 			</div>
-		</button>
+		</div>
 	</div>
 </div>
