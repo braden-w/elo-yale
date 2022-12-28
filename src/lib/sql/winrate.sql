7e67caa6c499bd6b2f29bec330941c6267cd5306
Braden Wong <bmw02002turbo@gmail.com>
2022-12-28 02:14:28 -0800

    Coalesce

--- src/lib/sql/winrate.sql
+++ src/lib/sql/winrate.sql
@@ -1,4 +1,4 @@
-SELECT w.winner, w.wins, l.loser, l.losses, (w.wins * 100.0 / (w.wins + l.losses)) as win_rate
+SELECT COALESCE(w.winner, l.loser) as college, COALESCE(w.wins, 0) as wins, COALESCE(l.losses, 0) as losses, (w.wins * 100.0 / (w.wins + l.losses)) as win_rate
 FROM (SELECT winner, COUNT(*) as wins FROM votes GROUP BY winner) w
 FULL JOIN (SELECT loser, COUNT(*) as losses FROM votes GROUP BY loser) l
   ON w.winner = l.loser;
