82be47cd1e255f9cd13d4194115da5ee0567e137
Braden Wong <bmw02002turbo@gmail.com>
2022-12-28 12:26:07 -0800

    Formatting

--- src/lib/types/DatabaseDefinitions.ts
+++ src/lib/types/DatabaseDefinitions.ts
@@ -1,53 +1,47 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
-  public: {
-    Tables: {
-      votes: {
-        Row: {
-          id: string
-          created_at: string | null
-          winner: string
-          loser: string
-          user_id: string | null
-        }
-        Insert: {
-          id: string
-          created_at?: string | null
-          winner: string
-          loser: string
-          user_id?: string | null
-        }
-        Update: {
-          id?: string
-          created_at?: string | null
-          winner?: string
-          loser?: string
-          user_id?: string | null
-        }
-      }
-    }
-    Views: {
-      leaderboard: {
-        Row: {
-          college: string | null
-          wins: number | null
-          losses: number | null
-          win_rate: number | null
-        }
-      }
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-  }
+	public: {
+		Tables: {
+			votes: {
+				Row: {
+					id: string;
+					created_at: string | null;
+					winner: string;
+					loser: string;
+					user_id: string | null;
+				};
+				Insert: {
+					id: string;
+					created_at?: string | null;
+					winner: string;
+					loser: string;
+					user_id?: string | null;
+				};
+				Update: {
+					id?: string;
+					created_at?: string | null;
+					winner?: string;
+					loser?: string;
+					user_id?: string | null;
+				};
+			};
+		};
+		Views: {
+			leaderboard: {
+				Row: {
+					college: string | null;
+					wins: number | null;
+					losses: number | null;
+					win_rate: number | null;
+				};
+			};
+		};
+		Functions: {
+			[_ in never]: never;
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+	};
 }
