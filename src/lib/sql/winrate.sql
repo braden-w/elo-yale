5ac73cf5fdcdec2f587c085d2a9a3cd1a73f97b8
Braden Wong <bmw02002turbo@gmail.com>
2022-12-28 02:15:34 -0800

    Working Winrate Calculator

--- src/lib/sql/winrate.sql
+++ src/lib/sql/winrate.sql
@@ -1,4 +1,26 @@
-SELECT COALESCE(w.winner, l.loser) as college, COALESCE(w.wins, 0) as wins, COALESCE(l.losses, 0) as losses, (w.wins * 100.0 / (w.wins + l.losses)) as win_rate
-FROM (SELECT winner, COUNT(*) as wins FROM votes GROUP BY winner) w
-FULL JOIN (SELECT loser, COUNT(*) as losses FROM votes GROUP BY loser) l
-  ON w.winner = l.loser;
+SELECT
+	COALESCE(w.winner, l.loser) AS college,
+	COALESCE(w.wins, 0) AS wins,
+	COALESCE(l.losses, 0) AS losses,
+	(
+		COALESCE(w.wins, 0) * 100.0 / (COALESCE(w.wins, 0) + COALESCE(l.losses, 0))
+	) AS win_rate
+FROM
+	(
+		SELECT
+			winner,
+			COUNT(*) AS wins
+		FROM
+			votes
+		GROUP BY
+			winner
+	) w FULL
+	JOIN (
+		SELECT
+			loser,
+			COUNT(*) AS losses
+		FROM
+			votes
+		GROUP BY
+			loser
+	) l ON w.winner = l.loser;
