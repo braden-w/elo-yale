9d85823350d60ec3c3f5990c75229d6dde17d5b2
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 23:34:46 -0800

    Use LayoutData

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,15 @@
+import { trpc } from '$lib/trpc/client';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
 	const { session } = await getSupabase(event);
-	return { session };
+	if (!session) {
+		throw redirect(303, '/');
+	}
+	const user_id = session.user.id;
+
+	const remainingCollegePairs = await trpc(event).getRemainingVotes.query(user_id);
+	return { session, remainingCollegePairs };
 };
