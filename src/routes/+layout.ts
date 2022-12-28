403478e1fef895664a63caae6d974c629642a8b8
Braden Wong <bmw02002turbo@gmail.com>
2022-12-28 01:33:55 -0800

    Redirect on email

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,16 @@
 import { trpc } from '$lib/trpc/client';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
-	const { session } = await getSupabase(event);
+	const { session, supabaseClient } = await getSupabase(event);
+	// If the email of the signed in user does not end with @yale.edu, sign them out
+	if (session?.user.email?.endsWith('@yale.edu') === false) {
+		await supabaseClient.auth.signOut();
+		throw redirect(303, '/');
+		alert('You must sign in with a Yale email address');
+	}
 	// if (!session) {
 	// 	throw redirect(303, '/');
 	// }
