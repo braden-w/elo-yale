d74acdaa9a3de0e62a938d07e3c9156df2e55c1d
Braden Wong <bmw02002turbo@gmail.com>
2022-12-27 21:18:08 -0800

    Auth Guard for Yale email address

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,1 +1,9 @@
 import '$lib/supabaseClient';
+// Auth Guard
+import { supabase } from '$lib/supabaseClient';
+// If the email of the signed in user does not end with @yale.edu, sign them out
+const { data } = await supabase.auth.getSession();
+if (data.session?.user.email?.endsWith('@yale.edu') === false) {
+	await supabase.auth.signOut();
+	alert('You must sign in with a Yale email address');
+}
