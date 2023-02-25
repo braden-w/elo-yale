b11809779484158df9ac02f5647ae3b233963e7d
Braden Wong <bmw02002turbo@gmail.com>
2023-02-24 21:29:10 -0500

    feat: My Past Votes

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -50,7 +50,7 @@
 
 <!-- {JSON.stringify(voteHistory)} -->
 <div class="mx-auto flex h-full flex-col items-center gap-6 p-6 sm:max-w-4xl">
-	<h1 class="text-3xl tracking-wide text-slate-700">My Votes</h1>
+	<h1 class="text-3xl tracking-wide text-slate-700">My Past Votes</h1>
 	{#if voteHistory.length === 0}
 		<p class="text-center text-slate-500">You haven't voted on any matchups yet!</p>
 		<p class="text-center text-slate-500">
