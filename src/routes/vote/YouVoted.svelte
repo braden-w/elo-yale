0b4a51bcb045596bee17135c3d56acdfb9f91679
Braden Wong <bmw02002turbo@gmail.com>
2023-02-24 21:11:56 -0500

    refactor: you voted!

---
+++ src/routes/vote/YouVoted.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+</script>
+
+<div class="flex flex-col items-center gap-2">
+	<h1 class="text-3xl tracking-wide text-slate-700 sm:text-3xl">You've voted on all the pairs!</h1>
+	<h2 class="text-center text-xs tracking-wide text-slate-700">
+		Go to
+		<a href="/leaderboard">
+			<span class="text-sky-700 hover:text-sky-900">leaderboard</span>
+		</a>
+		or see your
+		<a href="/history"> <span class="text-sky-700 hover:text-sky-900">vote history</span></a>
+	</h2>
+</div>
