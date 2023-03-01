eabeb6bd3b0279d5aca726b9a4693215e3a93a7c
Braden Wong <bmw02002turbo@gmail.com>
2023-02-28 23:52:24 -0500

    feat; attempt square image

--- static/generateCards.py
+++ static/generateCards.py
@@ -23,7 +23,21 @@ for filename in os.listdir(input_folder):
         # Create a new blank rectangular image with the desired output size and fill color
         output_image = Image.new('RGB', (output_width, output_height), (255, 255, 255))
 
-        # Paste the square image onto the center of the rectangular image
+        # Calculate the aspect ratio of the square image
+        square_aspect_ratio = square_image.width / square_image.height
+
+        # Calculate the new width and height of the square image to fit inside the rectangular image
+        new_width = int(output_height * square_aspect_ratio)
+        new_height = output_height
+
+        # Resize the square image to the new size
+        square_image = square_image.resize((new_width, new_height))
+
+        # Calculate the center coordinates of the resized square image
+        square_center_x = square_image.width // 2
+        square_center_y = square_image.height // 2
+
+        # Paste the resized square image onto the center of the rectangular image
         output_image.paste(square_image, (output_width//2 - square_center_x, output_height//2 - square_center_y))
 
         # Draw the top and bottom text on the rectangular image using the Habibi font
@@ -39,3 +53,4 @@ for filename in os.listdir(input_folder):
         output_filename = os.path.splitext(filename)[0] + '_rectangular.jpg'
         output_path = os.path.join(input_folder, output_filename)
         output_image.save(output_path)
+
