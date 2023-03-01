106c50f172ab4fe80808f9663df7aa333663fd61
Braden Wong <bmw02002turbo@gmail.com>
2023-03-01 02:17:10 -0500

    Feat: Residential College select

--- src/routes/ResidentialCollegeModal.svelte
+++ src/routes/ResidentialCollegeModal.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { COLLEGES } from '$lib/colleges';
+	let college = '';
 </script>
 
 <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -36,27 +37,23 @@
 							Residential College
 						</h3>
 						<div class="mt-2">
-							<p class="text-sm text-gray-500">
-								Before you begin, please select your residential college
-							</p>
+							<label for="college" class="text-sm text-gray-500">
+								Please select your residential college
+							</label>
 						</div>
 					</div>
 				</div>
-				<div class="mt-5 sm:mt-6">
-					<div>
-						<label for="college" class="block text-sm font-medium text-gray-700">
-							Residential College
-						</label>
-						<select
-							id="college"
-							name="college"
-							class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-						>
-							{#each COLLEGES as college (college)}
-								<option>{college}</option>
-							{/each}
-						</select>
-					</div>
+				<div class="mt-3">
+					<select
+						id="college"
+						name="college"
+						class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+						bind:value={college}
+					>
+						{#each COLLEGES as college (college)}
+							<option>{college}</option>
+						{/each}
+					</select>
 
 					<button
 						type="button"
