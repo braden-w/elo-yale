cd8763d0ae973fe3380313a4e3257933b384dbc7
Braden Wong <bmw02002turbo@gmail.com>
2023-02-28 23:54:44 -0500

    feat: center image in square

--- static/generateCards.py
+++ static/generateCards.py
@@ -24,11 +24,11 @@ for filename in os.listdir(input_folder):
         output_image = Image.new('RGB', (output_width, output_height), (255, 255, 255))
 
         # Calculate the aspect ratio of the square image
-        square_aspect_ratio = square_image.width / square_image.height
+        square_aspect_ratio = square_image.height / square_image.width 
 
         # Calculate the new width and height of the square image to fit inside the rectangular image
-        new_width = int(output_height * square_aspect_ratio)
-        new_height = output_height
+        new_width = int(output_width * square_aspect_ratio)
+        new_height = output_width
 
         # Resize the square image to the new size
         square_image = square_image.resize((new_width, new_height))
