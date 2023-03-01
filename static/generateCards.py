d24bd880c1b7c3d52e7f2e453886fcaa08c576f4
Braden Wong <bmw02002turbo@gmail.com>
2023-03-01 00:04:53 -0500

    feat: new script adds opacity and text

--- static/generateCards.py
+++ static/generateCards.py
@@ -1,56 +1,36 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-# Define the font and size
-font = ImageFont.truetype('/Users/braden/Code/elo-yale/static/fonts/Habibi-Regular.ttf', size=40)
+# set the path of the folder containing square pictures
+folder_path = "/Users/braden/Code/elo-yale/static/Colleges"
 
-# Set the desired output width and height for the rectangular image
-output_width = 500
-output_height = 700
+# set the opacity of the black filter
+opacity = 0.5
 
-# Iterate through all files in the input folder
-input_folder = '/Users/braden/Code/elo-yale/static/Colleges'
-for filename in os.listdir(input_folder):
-    if filename.endswith('.jpg') or filename.endswith('.png'):
-        # Load the square image
-        input_path = os.path.join(input_folder, filename)
-        square_image = Image.open(input_path)
+# Set font and size for the text
+font = ImageFont.truetype("/Users/braden/Code/elo-yale/static/fonts/Habibi-Regular.ttf", size=40)
 
-        # Calculate the center coordinates of the square image
-        square_center_x = square_image.width // 2
-        square_center_y = square_image.height // 2
+# Iterate through all files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        # Open the image and create a copy
+        image = Image.open(os.path.join(folder_path, filename))
+        image_copy = image.copy()
 
-        # Create a new blank rectangular image with the desired output size and fill color
-        output_image = Image.new('RGB', (output_width, output_height), (255, 255, 255))
+        # Create a black rectangle with the same size as the image
+        black_layer = Image.new("RGBA", image.size, (0, 0, 0, int(255*opacity)))
 
-        # Calculate the aspect ratio of the square image
-        square_aspect_ratio = square_image.height / square_image.width 
+        # Apply the black filter to the copy of the image
+        image_copy.paste(black_layer, (0, 0), black_layer)
 
-        # Calculate the new width and height of the square image to fit inside the rectangular image
-        new_width = int(output_width * square_aspect_ratio)
-        new_height = output_width
+        # Add text in the center of the image
+        draw = ImageDraw.Draw(image_copy)
+        text = "Hopper College"
+        text_width, text_height = draw.textsize(text, font=font)
+        x = (image_copy.width - text_width) / 2
+        y = (image_copy.height - text_height) / 2
+        draw.text((x, y), text, font=font, fill=(255, 255, 255, 255))
 
-        # Resize the square image to the new size
-        square_image = square_image.resize((new_width, new_height))
-
-        # Calculate the center coordinates of the resized square image
-        square_center_x = square_image.width // 2
-        square_center_y = square_image.height // 2
-
-        # Paste the resized square image onto the center of the rectangular image
-        output_image.paste(square_image, (output_width//2 - square_center_x, output_height//2 - square_center_y))
-
-        # Draw the top and bottom text on the rectangular image using the Habibi font
-        draw = ImageDraw.Draw(output_image)
-        top_text = 'Top Text'
-        bottom_text = 'Bottom Text'
-        top_text_width, top_text_height = draw.textsize(top_text, font)
-        bottom_text_width, bottom_text_height = draw.textsize(bottom_text, font)
-        draw.text(((output_width - top_text_width) / 2, 20), top_text, font=font, fill=(0, 0, 0))
-        draw.text(((output_width - bottom_text_width) / 2, output_height - bottom_text_height - 20), bottom_text, font=font, fill=(0, 0, 0))
-
-        # Save the output image with a new filename in the same directory as the input image
-        output_filename = os.path.splitext(filename)[0] + '_rectangular.jpg'
-        output_path = os.path.join(input_folder, output_filename)
-        output_image.save(output_path)
-
+        # Save the modified image with a new filename
+        new_filename = os.path.splitext(filename)[0] + "_modified.png"
+        image_copy.save(os.path.join(folder_path, new_filename))
