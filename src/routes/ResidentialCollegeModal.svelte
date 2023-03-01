c3946b25829e4bde0b95de8348521d42b187408b
Braden Wong <bmw02002turbo@gmail.com>
2023-03-01 02:05:36 -0500

    feat: select

--- src/routes/ResidentialCollegeModal.svelte
+++ src/routes/ResidentialCollegeModal.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { COLLEGES } from '$lib/colleges';
+</script>
+
 <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 	<!--
     Background backdrop, show/hide based on modal state.
@@ -39,6 +43,21 @@
 					</div>
 				</div>
 				<div class="mt-5 sm:mt-6">
+					<div>
+						<label for="college" class="block text-sm font-medium text-gray-700">
+							Residential College
+						</label>
+						<select
+							id="college"
+							name="college"
+							class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+						>
+							{#each COLLEGES as college (college)}
+								<option>{college}</option>
+							{/each}
+						</select>
+					</div>
+
 					<button
 						type="button"
 						class="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
