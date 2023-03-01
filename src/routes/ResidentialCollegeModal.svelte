336e08e35752dc1499f5379682ca0838889b9ee7
Braden Wong <bmw02002turbo@gmail.com>
2023-03-01 02:03:47 -0500

    feat: remove image

--- src/routes/ResidentialCollegeModal.svelte
+++ src/routes/ResidentialCollegeModal.svelte
@@ -27,25 +27,13 @@
 				class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
 			>
 				<div>
-					<div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-						<svg
-							class="h-6 w-6 text-green-600"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-						</svg>
-					</div>
 					<div class="mt-3 text-center sm:mt-5">
 						<h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">
-							Payment successful
+							Residential College
 						</h3>
 						<div class="mt-2">
 							<p class="text-sm text-gray-500">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
+								Before you begin, please select your residential college
 							</p>
 						</div>
 					</div>
