a3b875719125616f379438aa4af1783ddc662c08
Braden Wong <bmw02002turbo@gmail.com>
2023-02-28 22:54:56 -0500

    feat: do while implementation

--- src/routes/vote/TwoPictureComparison.svelte
+++ src/routes/vote/TwoPictureComparison.svelte
@@ -5,22 +5,37 @@
 	import HorizontalDivider from '$lib/components/HorizontalDivider.svelte';
 	import VerticalDivider from '$lib/components/VerticalDivider.svelte';
 	import {
-		ALL_COLLEGE_PAIRS,
 		collegeToImage,
 		TOTAL_NUMBER_PAIRS,
 		type College,
+		type CollegePair,
 		type CollegePairs
 	} from '$lib/colleges';
 	import { trpc } from '$lib/trpc/client';
 	import { page } from '$app/stores';
 
-	export let remainingCollegePairs: CollegePairs;
-	let pairsVotedThisSession = new Set<CollegePair>();
 	// How many someone has voted on, as a number from 0 to 91 (Can have not voted, or on all 90 pairs)
 	export let numberVotedSoFar: number;
+	export let remainingCollegePairs: CollegePairs;
 
-	let [collegeOne, collegeTwo] = remainingCollegePairs[0];
-	$: progress = (numberVotedSoFar / totalNumberPairs) * 100;
+	let pairsVotedThisSession = new Set<CollegePair>();
+	let currentCollegePair: CollegePair;
+
+	function getUnvotedCollegePair() {
+		// Get a random college pair from remainingCollegePairs that isn't in pairsVotedThisSession
+		let collegePair: CollegePair;
+		do {
+			const randomIndex = Math.floor(Math.random() * remainingCollegePairs.length);
+			collegePair = remainingCollegePairs[randomIndex];
+		} while (pairsVotedThisSession.has(collegePair));
+		return collegePair;
+	}
+
+	currentCollegePair = getUnvotedCollegePair();
+	pairsVotedThisSession.add(currentCollegePair);
+
+	$: [collegeOne, collegeTwo] = currentCollegePair;
+	$: progress = (numberVotedSoFar / TOTAL_NUMBER_PAIRS) * 100;
 	let animate = true;
 
 	// When the number 1 is pressed, the first college is the winner
@@ -51,6 +66,8 @@
 			},
 			{ position: 'top-right' }
 		);
+		currentCollegePair = getUnvotedCollegePair();
+		pairsVotedThisSession.add(currentCollegePair);
 		numberVotedSoFar++;
 	}
 </script>
